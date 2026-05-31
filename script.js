98cf5e062a4f23bf184ba3fd1f4b601b562467db
Nwachukwu Caleb <caleb1311c0de@gmail.com>
2026-05-31 05:24:23 +0100

    Made a few changes in the mobile version

--- script.js
+++ script.js
@@ -1,33 +1,4 @@
-new Swiper('.image-box-flex', {
-  loop: true,
-  spaceBetween: 30,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  breakpoints: {
-    0: {
-      slidesPerView: 1 // Fixed typo here
-    },
-    768: {
-      slidesPerView: 1 // Fixed typo here
-    },
-    1024: {
-      slidesPerView: 2 // Fixed typo here
-    },
-  }
-});
-const btn = document.getElementById("down");
-btn.addEventListener("click", () => {
-  window.scrollTo({
-    top: document.body.scrollHeight,
-    behavior: "smooth"
-  });
-});
+
 
 const cta = document.getElementById("cta");
 const text = document.getElementById("text");
